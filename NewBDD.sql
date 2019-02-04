cfad695e4f314697ad055295f6e2f98776083da5
Hugo Schindelman <hugo.schindelman@icloud.com>
2019-02-04 11:49:47 +0100

    correction du fichier NewBDD

--- NewBDD.sql
+++ NewBDD.sql
@@ -8,19 +8,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema quizcodac
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema quizcodac
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `quizcodac` DEFAULT CHARACTER SET utf8 ;
+USE `quizcodac` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `quizcodac`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`users` (
+CREATE TABLE IF NOT EXISTS `quizcodac`.`users` (
   `idusers` INT NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(50) NOT NULL,
   `prenom` VARCHAR(50) NOT NULL,
@@ -37,9 +37,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`quiz`
+-- Table `quizcodac`.`quiz`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`quiz` (
+CREATE TABLE IF NOT EXISTS `quizcodac`.`quiz` (
   `idquiz` INT NOT NULL AUTO_INCREMENT,
   `duree` VARCHAR(45) NOT NULL,
   `description` VARCHAR(255) NOT NULL,
@@ -50,9 +50,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`questions`
+-- Table `quizcodac`.`questions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`questions` (
+CREATE TABLE IF NOT EXISTS `quizcodac`.`questions` (
   `idquestions` INT NOT NULL AUTO_INCREMENT,
   `question` VARCHAR(255) NOT NULL,
   `reponse1` VARCHAR(255) NOT NULL,
@@ -68,16 +68,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`questions` (
   INDEX `fk_questions_quiz1_idx` (`quiz_idquiz` ASC),
   CONSTRAINT `fk_questions_quiz1`
     FOREIGN KEY (`quiz_idquiz`)
-    REFERENCES `mydb`.`quiz` (`idquiz`)
+    REFERENCES `quizcodac`.`quiz` (`idquiz`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`tags`
+-- Table `quizcodac`.`tags`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`tags` (
+CREATE TABLE IF NOT EXISTS `quizcodac`.`tags` (
   `idtags` INT NOT NULL AUTO_INCREMENT,
   `nom` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idtags`))
@@ -85,9 +85,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`scores`
+-- Table `quizcodac`.`scores`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`scores` (
+CREATE TABLE IF NOT EXISTS `quizcodac`.`scores` (
   `idscores` INT NOT NULL AUTO_INCREMENT,
   `userans` VARCHAR(255) NOT NULL,
   `temps` VARCHAR(45) NOT NULL,
@@ -100,21 +100,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`scores` (
   INDEX `fk_scores_questions1_idx` (`questions_idquestions` ASC, `questions_quiz_idquiz` ASC),
   CONSTRAINT `fk_scores_users1`
     FOREIGN KEY (`users_idusers`)
-    REFERENCES `mydb`.`users` (`idusers`)
+    REFERENCES `quizcodac`.`users` (`idusers`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_scores_questions1`
     FOREIGN KEY (`questions_idquestions` , `questions_quiz_idquiz`)
-    REFERENCES `mydb`.`questions` (`idquestions` , `quiz_idquiz`)
+    REFERENCES `quizcodac`.`questions` (`idquestions` , `quiz_idquiz`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`commentaires`
+-- Table `quizcodac`.`commentaires`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`commentaires` (
+CREATE TABLE IF NOT EXISTS `quizcodac`.`commentaires` (
   `idcommentaires` INT NOT NULL AUTO_INCREMENT,
   `commentaire` TEXT NOT NULL,
   `users_idusers` INT NOT NULL,
@@ -124,21 +124,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`commentaires` (
   INDEX `fk_commentaires_quiz1_idx` (`quiz_idquiz` ASC),
   CONSTRAINT `fk_commentaires_users1`
     FOREIGN KEY (`users_idusers`)
-    REFERENCES `mydb`.`users` (`idusers`)
+    REFERENCES `quizcodac`.`users` (`idusers`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_commentaires_quiz1`
     FOREIGN KEY (`quiz_idquiz`)
-    REFERENCES `mydb`.`quiz` (`idquiz`)
+    REFERENCES `quizcodac`.`quiz` (`idquiz`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`users_has_quiz`
+-- Table `quizcodac`.`users_has_quiz`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`users_has_quiz` (
+CREATE TABLE IF NOT EXISTS `quizcodac`.`users_has_quiz` (
   `users_idusers` INT NOT NULL,
   `quiz_idquiz` INT NOT NULL,
   `quiz_done` TINYINT(1) NOT NULL DEFAULT 0,
@@ -147,21 +147,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`users_has_quiz` (
   INDEX `fk_users_has_quiz_users_idx` (`users_idusers` ASC),
   CONSTRAINT `fk_users_has_quiz_users`
     FOREIGN KEY (`users_idusers`)
-    REFERENCES `mydb`.`users` (`idusers`)
+    REFERENCES `quizcodac`.`users` (`idusers`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_has_quiz_quiz1`
     FOREIGN KEY (`quiz_idquiz`)
-    REFERENCES `mydb`.`quiz` (`idquiz`)
+    REFERENCES `quizcodac`.`quiz` (`idquiz`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`tags_has_questions`
+-- Table `quizcodac`.`tags_has_questions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`tags_has_questions` (
+CREATE TABLE IF NOT EXISTS `quizcodac`.`tags_has_questions` (
   `tags_idtags` INT NOT NULL,
   `questions_idquestions` INT NOT NULL,
   `questions_quiz_idquiz` INT NOT NULL,
@@ -170,12 +170,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`tags_has_questions` (
   INDEX `fk_tags_has_questions_tags1_idx` (`tags_idtags` ASC),
   CONSTRAINT `fk_tags_has_questions_tags1`
     FOREIGN KEY (`tags_idtags`)
-    REFERENCES `mydb`.`tags` (`idtags`)
+    REFERENCES `quizcodac`.`tags` (`idtags`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_tags_has_questions_questions1`
     FOREIGN KEY (`questions_idquestions` , `questions_quiz_idquiz`)
-    REFERENCES `mydb`.`questions` (`idquestions` , `quiz_idquiz`)
+    REFERENCES `quizcodac`.`questions` (`idquestions` , `quiz_idquiz`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
