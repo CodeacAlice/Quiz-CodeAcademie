9a2b7faebd965c199680fc9356c08bee385bdaab
CodeacAlice <alice.carry@lilo.org>
2020-07-07 21:00:12 +0200

    [html/css] icône pour menu ajoutée à header

--- assets/views/header.php
+++ assets/views/header.php
@@ -1,4 +1,11 @@
 <header>
-    <img id="logo_codeac" src="<?= $path ?>/assets/images/Logo-code-academie.jpg" alt="Logo de la Code Académie">
-    <img id="logo_face" src="<?= $path ?>/assets/images/RENNES_PNG.png" alt="Logo de FACE Rennes">
+    <div style="width:100%;">
+        <img id="logo_codeac" src="<?= $path ?>/assets/images/Logo-code-academie.jpg" alt="Logo de la Code Académie">
+        <img id="logo_face" src="<?= $path ?>/assets/images/RENNES_PNG.png" alt="Logo de FACE Rennes">
+    </div>
+    <?php if ($_SESSION) { ?>
+    <div>
+        <?php include($path."/assets/images/accountbutton.svg"); ?>
+    </div>
+    <?php } ?>
 </header>
