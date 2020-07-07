61e3b1aaf5e837a9a089a4bc40d80148b29c5778
CodeacAlice <alice.carry@lilo.org>
2020-07-07 20:09:48 +0200

    [refactoring] retouche css header

--- assets/views/header.php
+++ assets/views/header.php
@@ -1,10 +1,4 @@
 <header>
-    <div class="banner">
-        <span class="code_ac">
-            <img src="<?= $path ?>/assets/images/Logo-code-academie.jpg" alt="Logo de la Code Académie" style="width:30%;">
-        </span>
-        <span class="face">
-            <img src="<?= $path ?>/assets/images/RENNES_PNG.png" alt="Logo de FACE Rennes" style="width:35%;">
-        </span>
-    </div>
+    <img id="logo_codeac" src="<?= $path ?>/assets/images/Logo-code-academie.jpg" alt="Logo de la Code Académie">
+    <img id="logo_face" src="<?= $path ?>/assets/images/RENNES_PNG.png" alt="Logo de FACE Rennes">
 </header>
--- index.php
+++ index.php
@@ -14,8 +14,9 @@
   <!-- jQuery -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-  <title>[Code Academie] Promo #3 - Quiz - Connection</title>
+  <title>[Code Academie] Promo #3 - Quiz - Connexion</title>
   <link rel="stylesheet" type="text/css" href="./assets/css/base.css">
+  <link rel="stylesheet" type="text/css" href="./assets/css/stylesheet.css">
 </head>
 
 <body>
@@ -61,17 +62,6 @@
     }
     ?>
 
-
-  <!-- <header>
-    <div class="banner">
-      <span class="code_ac">
-        <img src="./assets/images/Logo-code-academie.jpg" alt="Logo de la Code Académie" style="width:30%;">
-      </span>
-      <span class="face">
-        <img src="./assets/images/RENNES_PNG.png" alt="Logo de FACE Rennes" style="width:35%;">
-      </span>
-    </div>
-  </header> -->
   <?php $path = '.'; include($path."/assets/views/header.php"); ?>
 
   <section class="page">
@@ -89,7 +79,6 @@
   </section>
 
 
- <script type="text/javascript" src="script.js"></script>
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
