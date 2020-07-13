b7e02c1df5b95bcc690e58ceb3f563789eef3c25
CodeacAlice <alice.carry@lilo.org>
2020-07-13 22:07:24 +0200

    [feature] création view footer

---
+++ assets/views/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <div>
+        Icônes par (liens) sur flaticon<br>
+        Site réalisé par Audrey Gonthier, Hugo Schindelman et Alice Carry, tous droits réservés. 
+    </div>
+</footer>
