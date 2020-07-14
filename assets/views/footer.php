377ce1673fa8b9152d50190fe3c1a515383bcd8e
CodeacAlice <alice.carry@lilo.org>
2020-07-14 17:36:20 +0200

    [html] liens ajoutés au footer

--- assets/views/footer.php
+++ assets/views/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div>
-        Icônes par (liens) sur flaticon<br>
+        Icônes par <a href="https://www.flaticon.com/authors/freepik" target="_blank">Freepik</a>, <a href="https://www.flaticon.com/authors/chanut" target="_blank">Chanut</a> et <a href="https://www.flaticon.com/authors/dave-gandy" target="_blank">Dave Gandy</a> sur <a href="https://www.flaticon.com/" target="_blank">Flaticon</a><br>
         Site réalisé par Audrey Gonthier, Hugo Schindelman et Alice Carry, tous droits réservés. 
     </div>
 </footer>
