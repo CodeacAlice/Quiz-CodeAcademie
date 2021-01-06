13cd9047ef28aa846cca8c72a71623f8103378f0
Alice Carry <43727761+CodeacAlice@users.noreply.github.com>
2021-01-06 12:18:01 +0100

    head.php ajouté à questions.php

--- pages/admin/questions.php
+++ pages/admin/questions.php
@@ -1,26 +1,20 @@
-<?php session_start(); $path = "../.."; ?>
-<!doctype html>
-<html lang="fr">
+<?php 
+  session_start(); 
+  $path = "../.."; $titlepage = "Liste des questions";
 
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  require_once $path.'/database.php';
+  if(!isset($_SESSION['Admin'])){
+    header('location:../deconnexion.php');
+  }
+  else {
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-	<!-- FontAwesome CSS -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
-	<!-- jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<title>[Code Academie] Promo #3 - Liste des questions</title>
-
-	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="../../assets/css/stylesheet.css">
+    include($path."/assets/views/head.php"); 
+  
+?>
+	<!-- CSS additionnel -->
 	<link rel="stylesheet" type="text/css" href="../../assets/css/questions.css">
 
+	<!-- Code Javascript pour modifier une question -->
 	<script type="text/javascript">
 		function modifQuest(idquest, idquiz) {
 			if (idquest == 0) {
@@ -46,19 +40,6 @@
 		}
 	</script>
 
-
-</head>
-
-<body>
-	<?php
-	require_once '../../database.php';
-	if(!isset($_SESSION['Admin'])){
-		header('location:../deconnexion.php');
-	}
-	else {
-	?>
-
-	<?php include($path."/assets/views/header.php"); ?>
 
 	<!-- Ajouter une question -->
 	<div class="modal fade" id="modalAjout" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -323,9 +304,7 @@
 
 	<?php include($path."/assets/views/footer.php"); ?>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<?php }?>
 </body>
 </html>
+
+<?php }?>
