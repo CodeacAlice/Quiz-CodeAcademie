8c148441cf4cb78800d1f83d293b0fced8256bf0
Alice Carry <43727761+CodeacAlice@users.noreply.github.com>
2021-01-06 11:11:16 +0100

    [refactoring] import Boostrap, jquery et popper en local sur pages index et homepage admin

--- index.php
+++ index.php
@@ -7,12 +7,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 
-  <!-- FontAwesome CSS -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
   <!-- jQuery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="assets/js/jquery.js"></script>
 
   <title>[Code Academie] Promo #3 - Quiz - Connexion</title>
 
@@ -57,7 +55,7 @@
 
         }
         else{
-          $error = 'identifiants incorrects.';
+          $error = 'Identifiants incorrects.';
         }
       }
       if(isset($error)){
@@ -66,7 +64,7 @@
         </div>';
       }
       else {
-        $error = 'Veuillez remplir tout les champs.';
+        $error = 'Veuillez remplir tous les champs.';
       }
     }
     ?>
@@ -87,11 +85,6 @@
 
 
   <?php include($path."/assets/views/footer.php"); ?>
-
-
- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
