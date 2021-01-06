62942b3618092e69afb27109161361a003103f40
Alice Carry <43727761+CodeacAlice@users.noreply.github.com>
2021-01-06 13:22:36 +0100

    Ajout de Fontawesome dans le footer

--- assets/views/footer.php
+++ assets/views/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div>
-        Icônes par <a href="https://www.flaticon.com/authors/freepik" target="_blank">Freepik</a>, <a href="https://www.flaticon.com/authors/chanut" target="_blank">Chanut</a> et <a href="https://www.flaticon.com/authors/dave-gandy" target="_blank">Dave Gandy</a> sur <a href="https://www.flaticon.com/" target="_blank">Flaticon</a><br>
+        Icônes par <a href="https://www.flaticon.com/authors/freepik" target="_blank">Freepik</a>, <a href="https://www.flaticon.com/authors/chanut" target="_blank">Chanut</a> et <a href="https://www.flaticon.com/authors/dave-gandy" target="_blank">Dave Gandy</a> sur <a href="https://www.flaticon.com/" target="_blank">Flaticon</a>, et <a href="https://fontawesome.com/license" target="_blank">Fontawesome</a><br>
         Site réalisé par Audrey Gonthier, Hugo Schindelman et Alice Carry, tous droits réservés. 
     </div>
 </footer>
