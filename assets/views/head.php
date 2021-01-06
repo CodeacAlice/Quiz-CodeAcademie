0806bb2e6a685402185ab68e38a83ec478ac6455
Alice Carry <43727761+CodeacAlice@users.noreply.github.com>
2021-01-06 11:38:25 +0100

    Create head.php

---
+++ assets/views/head.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="<?= $path ?>/assets/css/bootstrap.min.css">
+
+  <!-- jQuery -->
+  <script src="<?= $path ?>/assets/js/jquery.js"></script>
+
+  <title><?= $titlepage ?> - [Code Academie] Promo #3 - Quiz</title>
+
+  <!-- CSS -->
+  <link rel="stylesheet" type="text/css" href="<?= $path ?>/assets/css/stylesheet.css">
+  <link rel="stylesheet" type="text/css" href="<?= $path ?>/assets/css/homepage.css">
+  
+</head>
