c943e7ccf3f2542f774b92b023cc0c36fc8ff518
Alice Carry <43727761+CodeacAlice@users.noreply.github.com>
2021-01-06 12:44:20 +0100

    head.php ajouté à tags.php + icône poubelle en local

--- pages/admin/tags.php
+++ pages/admin/tags.php
@@ -1,247 +1,228 @@
-<?php session_start(); $path = "../.."; ?>
-<!doctype html>
-<html lang="fr">
+<?php 
+  session_start(); 
+  $path = "../.."; $titlepage = "Liste des tags";
 
-<head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  require_once $path.'/database.php';
+  if(!isset($_SESSION['Admin'])){
+    header('location:../deconnexion.php');
+  }
+  else {
 
-	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    include($path."/assets/views/head.php"); 
+  
+?>
+<!-- CSS additionnel -->
+<link rel="stylesheet" type="text/css" href="../../assets/css/tags.css">
 
-	<!-- FontAwesome CSS -->
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
-	<!-- jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<title>[Code Academie] Promo #3 - Liste des tags</title>
-
-	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="../../assets/css/stylesheet.css">
-	<link rel="stylesheet" type="text/css" href="../../assets/css/tags.css">
-
-	<script type="text/javascript">
-		function modifTag(idtag, idquiz) {
-			if (idtag == 0) {
-				document.getElementById("modifTag").innerHTML = "";
-				return;
+<!-- Code JavaScript pour modifier et supprimer un tag -->
+<script type="text/javascript">
+	function modifTag(idtag, idquiz) {
+		if (idtag == 0) {
+			document.getElementById("modifTag").innerHTML = "";
+			return;
+		} else {
+			if (window.XMLHttpRequest) {
+				// code for IE7+, Firefox, Chrome, Opera, Safari
+				xmlhttp = new XMLHttpRequest();
 			} else {
-				if (window.XMLHttpRequest) {
-		            // code for IE7+, Firefox, Chrome, Opera, Safari
-		            xmlhttp = new XMLHttpRequest();
-		        } else {
-		            // code for IE6, IE5
-		            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		        }
-		        xmlhttp.onreadystatechange = function() {
-		        	if (this.readyState == 4 && this.status == 200) {
-		        		document.getElementById("modifTag").innerHTML = this.responseText;
-		        		$('#modalModif').modal('show');
-		        	}
-		        };
-		        xmlhttp.open("GET","../../assets/php/modifTag.php?t="+idtag+"&q="+idquiz,true);
-		        xmlhttp.send();
-		    }
+				// code for IE6, IE5
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					document.getElementById("modifTag").innerHTML = this.responseText;
+					$('#modalModif').modal('show');
+				}
+			};
+			xmlhttp.open("GET","../../assets/php/modifTag.php?t="+idtag+"&q="+idquiz,true);
+			xmlhttp.send();
 		}
-		
-		function deleteTag(idtag, idquiz) {
-			if (idtag == 0) {
-				document.getElementById("supprTag").innerHTML = "";
-				return;
+	}
+	
+	function deleteTag(idtag, idquiz) {
+		if (idtag == 0) {
+			document.getElementById("supprTag").innerHTML = "";
+			return;
+		} else {
+			if (window.XMLHttpRequest) {
+				// code for IE7+, Firefox, Chrome, Opera, Safari
+				xmlhttp = new XMLHttpRequest();
 			} else {
-				if (window.XMLHttpRequest) {
-		            // code for IE7+, Firefox, Chrome, Opera, Safari
-		            xmlhttp = new XMLHttpRequest();
-		        } else {
-		            // code for IE6, IE5
-		            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		        }
-		        xmlhttp.onreadystatechange = function() {
-		        	if (this.readyState == 4 && this.status == 200) {
-		        		document.getElementById("supprTag").innerHTML = this.responseText;
-		        		$('#modalDelete').modal('show');
-		        	}
-		        };
-		        xmlhttp.open("GET","../../assets/php/supprTag.php?t="+idtag+"&q="+idquiz,true);
-		        xmlhttp.send();
-		    }
+				// code for IE6, IE5
+				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			xmlhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					document.getElementById("supprTag").innerHTML = this.responseText;
+					$('#modalDelete').modal('show');
+				}
+			};
+			xmlhttp.open("GET","../../assets/php/supprTag.php?t="+idtag+"&q="+idquiz,true);
+			xmlhttp.send();
 		}
-	</script>
-</head>
-
-<body>
-	<?php
-	require_once '../../database.php';
-	if(!isset($_SESSION['Admin'])){
-	    header('location:../deconnexion.php');
 	}
-	else {
-	?>
+</script>
 
-	<!-- Modal pour ajouter un tag -->
-	<div class="modal fade" id="modalAjout" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<div class="modal-title" id="exampleModalLabel"><b>Ajouter un tag</b></div>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-					<?php echo '<form action="tags.php?idquiz='.$_GET["idquiz"].'" method="post">'; ?>
-						<p>Nom : <input type="text" name="nom" required maxlength="45"></p>
-						<input type="submit" name="add" value="Ajouter" class="btnface">
-					</form>
-				</div>
+
+<!-- Modal pour ajouter un tag -->
+<div class="modal fade" id="modalAjout" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<div class="modal-title" id="exampleModalLabel"><b>Ajouter un tag</b></div>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<?php echo '<form action="tags.php?idquiz='.$_GET["idquiz"].'" method="post">'; ?>
+					<p>Nom : <input type="text" name="nom" required maxlength="45"></p>
+					<input type="submit" name="add" value="Ajouter" class="btnface">
+				</form>
 			</div>
 		</div>
 	</div>
-	<?php
-	// Code pour ajouter un tag
-	if (isset($_POST['add']) && $_POST['add'] == 'Ajouter') {
+</div>
+<?php
+// Code pour ajouter un tag
+if (isset($_POST['add']) && $_POST['add'] == 'Ajouter') {
 
-		$nom = str_replace("'", "\'", $_POST['nom']);
+	$nom = str_replace("'", "\'", $_POST['nom']);
 
-		$sth = $bdd->prepare("INSERT INTO tags (nom) VALUES ('".$nom."')");
-		$sth->execute();
-	}
-	?>
+	$sth = $bdd->prepare("INSERT INTO tags (nom) VALUES ('".$nom."')");
+	$sth->execute();
+}
+?>
 
-	<!-- Modal pour modifier un tag -->
-	<div class="modal fade" id="modalModif" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content" id="modifTag">
-				<div class="modal-header">
-					<div class="modal-title" id="exampleModalLabel"><b>Modifier le tag nom_tag</b></div>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-					<?php echo '<form action="tags.php?idquiz='.$_GET["idquiz"].'" method="post">'; ?>
-						<p style="display:none">id : <input type="text" name="id" required value="0"></p>
-						<p>Nom : <input type="text" name="nom" required maxlength="45" value="nom_tag"></p>
-						<input type="submit" name="update" value="Modifier" class="btnface-small">
-					</form>
-				</div>
+<!-- Modal pour modifier un tag -->
+<div class="modal fade" id="modalModif" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content" id="modifTag">
+			<div class="modal-header">
+				<div class="modal-title" id="exampleModalLabel"><b>Modifier le tag nom_tag</b></div>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+				<?php echo '<form action="tags.php?idquiz='.$_GET["idquiz"].'" method="post">'; ?>
+					<p style="display:none">id : <input type="text" name="id" required value="0"></p>
+					<p>Nom : <input type="text" name="nom" required maxlength="45" value="nom_tag"></p>
+					<input type="submit" name="update" value="Modifier" class="btnface-small">
+				</form>
 			</div>
 		</div>
 	</div>
-	<?php
-	// Code pour modifier un tag
-	if (isset($_POST['update']) && $_POST['update'] == 'Modifier') {
+</div>
+<?php
+// Code pour modifier un tag
+if (isset($_POST['update']) && $_POST['update'] == 'Modifier') {
 
-		$id = $_POST['id'];
-		$nom = str_replace("'", "\'", $_POST['nom']);
+	$id = $_POST['id'];
+	$nom = str_replace("'", "\'", $_POST['nom']);
 
-		$sth = $bdd->prepare("UPDATE tags SET nom = '".$nom."' WHERE idtags = '".$id."';");
-		$sth->execute();
-	}
-	?>
+	$sth = $bdd->prepare("UPDATE tags SET nom = '".$nom."' WHERE idtags = '".$id."';");
+	$sth->execute();
+}
+?>
 
-	<!-- Modal pour supprimer un tag -->
-	<div class="modal fade" id="modalDelete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div class="modal-dialog modal-lg" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<div class="modal-title" id="exampleModalLabel"><b>ATTENTION</b></div>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body" id="supprTag">
-					<p>Voulez-vous vraiment supprimer définitivement le tag nom_tag ?</p>
-					<?php echo '<form action="tags.php?idquiz='.$_GET["idquiz"].'" method="post">'; ?>
-						<p style="display:none">id : <input type="text" name="idSupp" required value="0"></p>
-						<input type="submit" name="delete" value="Oui" class="btnface-small">
-						<button type="button" class="btnface-small" data-dismiss="modal" style="margin: auto;">Non</button>
-					</form>
-				</div>
+<!-- Modal pour supprimer un tag -->
+<div class="modal fade" id="modalDelete" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<div class="modal-title" id="exampleModalLabel"><b>ATTENTION</b></div>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body" id="supprTag">
+				<p>Voulez-vous vraiment supprimer définitivement le tag nom_tag ?</p>
+				<?php echo '<form action="tags.php?idquiz='.$_GET["idquiz"].'" method="post">'; ?>
+					<p style="display:none">id : <input type="text" name="idSupp" required value="0"></p>
+					<input type="submit" name="delete" value="Oui" class="btnface-small">
+					<button type="button" class="btnface-small" data-dismiss="modal" style="margin: auto;">Non</button>
+				</form>
 			</div>
 		</div>
 	</div>
-	<?php
-	// Code pour supprimer un tag
-	if (isset($_POST['delete']) && $_POST['delete'] == 'Oui') {
+</div>
+<?php
+// Code pour supprimer un tag
+if (isset($_POST['delete']) && $_POST['delete'] == 'Oui') {
 
-		$id = $_POST['idSupp'];
+	$id = $_POST['idSupp'];
 
-		$sth = $bdd->prepare("DELETE FROM tags_has_questions WHERE tags_idtags = '".$id."';
-			DELETE FROM tags WHERE idtags = '".$id."';");
+	$sth = $bdd->prepare("DELETE FROM tags_has_questions WHERE tags_idtags = '".$id."';
+		DELETE FROM tags WHERE idtags = '".$id."';");
+	$sth->execute();
+}
+?>
+
+
+
+
+<section>
+	<h2>Liste des tags et présence dans le quiz « <?php 
+		// Code pour afficher le nom du quiz
+		$sth = $bdd->prepare("SELECT * FROM quiz WHERE idquiz ='".$_GET["idquiz"]."'");
 		$sth->execute();
-	}
-	?>
+		$result = $sth->fetch();
+		echo $result['titre'];
+		?> » :
+	</h2>
+	<?php
+		// Code pour afficher tous les tags ainsi que les questions associées
+		$sth = $bdd->prepare('SELECT * FROM tags ORDER BY nom');
+		$sth->execute();
+		$result = $sth->fetchAll();
+		if($sth->rowCount()) {
+			foreach($result as $row){
+				$sth2 = $bdd->prepare("SELECT questions.numero FROM tags, tags_has_questions, questions
+					WHERE tags.idtags = tags_has_questions.tags_idtags
+					AND tags_has_questions.questions_idquestions = questions.idquestions
+					AND questions.quiz_idquiz = '".$_GET["idquiz"]."'
+					AND tags.idtags = '".$row['idtags']."';");
+				$sth2->execute();
+				$result2 = $sth2->fetchAll();
 
-
-	<?php include($path."/assets/views/header.php"); ?>
-
-	<section>
-		<h2>Liste des tags et présence dans le quiz « <?php 
-			// Code pour afficher le nom du quiz
-			$sth = $bdd->prepare("SELECT * FROM quiz WHERE idquiz ='".$_GET["idquiz"]."'");
-			$sth->execute();
-			$result = $sth->fetch();
-			echo $result['titre'];
-			?> » :
-		</h2>
-		<?php
-			// Code pour afficher tous les tags ainsi que les questions associées
-			$sth = $bdd->prepare('SELECT * FROM tags ORDER BY nom');
-			$sth->execute();
-			$result = $sth->fetchAll();
-			if($sth->rowCount()) {
-				foreach($result as $row){
-					$sth2 = $bdd->prepare("SELECT questions.numero FROM tags, tags_has_questions, questions
-						WHERE tags.idtags = tags_has_questions.tags_idtags
-						AND tags_has_questions.questions_idquestions = questions.idquestions
-						AND questions.quiz_idquiz = '".$_GET["idquiz"]."'
-						AND tags.idtags = '".$row['idtags']."';");
-					$sth2->execute();
-					$result2 = $sth2->fetchAll();
-
-					if($sth2->rowCount()) {
-						$check = '<i class="far fa-check-circle"></i> ';
-						$quests = '<span>Questions concernées : ';
-						foreach($result2 as $row2){
-							$quests .= $row2['numero'].' ';
-						}
-						$quests .= '</span>';
+				if($sth2->rowCount()) {
+					$check = '<i class="far fa-check-circle"></i> ';
+					$quests = '<span>Questions concernées : ';
+					foreach($result2 as $row2){
+						$quests .= $row2['numero'].' ';
 					}
-					else {$check = ''; $quests = '';}
-					?>
+					$quests .= '</span>';
+				}
+				else {$check = ''; $quests = '';}
+				?>
 
-					<div class="tag_name">
-						<div class="tag_example">
-							<div>
-								<?php echo $check.$row['nom']; ?>
-							</div>
-							<?= $quests ?>
-							<div>
-								<button class="btnface-small" onclick="modifTag(<?=$row['idtags']?>, <?=$_GET['idquiz']?>)">Modifier</button>
-								<button onclick="deleteTag(<?=$row['idtags']?>, <?=$_GET['idquiz']?>)">
-									<i class="fas fa-trash-alt"></i>
-								</button>
-							</div>
+				<div class="tag_name">
+					<div class="tag_example">
+						<div>
+							<?php echo $check.$row['nom']; ?>
+						</div>
+						<?= $quests ?>
+						<div>
+							<button class="btnface-small" onclick="modifTag(<?=$row['idtags']?>, <?=$_GET['idquiz']?>)">Modifier</button>
+							<button class="btnface-small" onclick="deleteTag(<?=$row['idtags']?>, <?=$_GET['idquiz']?>)">
+								<?php include($path."/assets/img/trash-alt-solid.svg"); ?>
+							</button>
 						</div>
 					</div>
-				<?php }
-			}
-			else {echo "Il n'y a pas encore de tag.";}
-		?>
-		
+				</div>
+			<?php }
+		}
+		else {echo "Il n'y a pas encore de tag.";}
+	?>
+	
 
-		<div class="my-5"><button data-toggle="modal" data-target="#modalAjout">Ajouter un tag</button></div>
-		<a class="btnface-small" href="mesquiz.php">Retour aux quiz</a>
-	</section>
+	<div class="my-5"><button data-toggle="modal" data-target="#modalAjout">Ajouter un tag</button></div>
+	<a class="btnface-small" href="mesquiz.php">Retour aux quiz</a>
+</section>
 
-	<?php include($path."/assets/views/footer.php"); ?>
+<?php include($path."/assets/views/footer.php"); ?>
 
-
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<?php } ?>
 </body>
 </html>
+<?php } ?>
