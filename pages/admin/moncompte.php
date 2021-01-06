434f612b5d53b148623188e937f066bb3819afcb
Alice Carry <43727761+CodeacAlice@users.noreply.github.com>
2021-01-06 12:13:06 +0100

    head.php ajouté sur moncompte.php

--- pages/admin/moncompte.php
+++ pages/admin/moncompte.php
@@ -1,37 +1,13 @@
-<?php session_start(); $path = "../.."; ?><!doctype html>
-<html lang="fr">
+<?php 
+  session_start(); 
+  $path = "../.."; $titlepage = "Mon compte";
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-  <!-- FontAwesome CSS -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
-  <!-- jQuery -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-  <title>[Code Academie] Promo #3 - Mon Compte</title>
-
-  <!-- CSS -->
-  <link rel="stylesheet" type="text/css" href="../../assets/css/stylesheet.css">
-
-</head>
-
-<body>
-  <?php
-  require_once '../../database.php';
+  require_once $path.'/database.php';
   if(!isset($_SESSION['Admin'])){
     header('location:../deconnexion.php');
   }
   else {
-  ?>
 
-
-  <?php
   // Code pour modifier les infos
   if (isset($_POST['update']) && $_POST['update'] == 'Modifier') {
     $oldpwd = str_replace("'", "\'", $_POST['oldpwd']);
@@ -53,10 +29,12 @@
       }
     }
   }
+
+  
+  include($path."/assets/views/head.php"); 
+
   ?>
 
-
-  <?php include($path."/assets/views/header.php"); ?>
 
   <section>
     <h2>Mon compte</h2>
@@ -72,6 +50,8 @@
 
     <button data-toggle="modal" data-target="#modalModif">Modifier mes informations</button>
   </section>
+
+
   <!-- Modal pour modifier les infos -->
   <div class="modal fade" id="modalModif" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
@@ -97,11 +77,8 @@
 
   <?php include($path."/assets/views/footer.php"); ?>
 
-
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <?php } ?>
 </body>
 </html>
+
+
+<?php } ?>
